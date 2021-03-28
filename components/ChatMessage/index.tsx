4cd58ed50c2eb2037541fefa9b9c0b3f71dc9efb
nedbrennan <nedbrnnn@gmail.com>
2021-03-28 11:05:04 -0400

    feat(ChatMessage): adds stying and structure to ChatMessage component

--- components/ChatMessage/index.tsx
+++ components/ChatMessage/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Message } from '../../types'
+import moment from 'moment'
+import styles from '../ChatListItem/style'
 
 
 export type ChatMessageProps = {
@@ -10,12 +12,14 @@ export type ChatMessageProps = {
 const ChatMessage = (props: ChatMessageProps) => {
     const { message } = props
     return (
-        <View>
+        <View style={styles.container}>
+            <View style={styles.messageBox}>
+            <Text style={styles.username}>{message.user.name}</Text>
+            <Text>{moment(message.createdAt).fromNow()}</Text>
             <Text>{message.content}</Text>
+            </View>
         </View>
     )
 }
 
 export default ChatMessage
-
-const styles = StyleSheet.create({})
