24c6829eb5ed03772027bdea71cd306ff3a87423
nedbrennan <nedbrnnn@gmail.com>
2021-03-28 11:56:36 -0400

    feat(ChatMessage): conditional styling added to messages

--- components/ChatMessage/index.tsx
+++ components/ChatMessage/index.tsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Message } from '../../types'
-import moment from 'moment'
-import styles from '../ChatListItem/style'
-
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Message } from '../../types';
+import moment from 'moment';
+import styles from './styles';
 
 export type ChatMessageProps = {
-    message: Message
-}
+  message: Message;
+};
 
 const ChatMessage = (props: ChatMessageProps) => {
-    const { message } = props
-    return (
-        <View style={styles.container}>
-            <View style={styles.messageBox}>
-            <Text style={styles.username}>{message.user.name}</Text>
-            <Text>{moment(message.createdAt).fromNow()}</Text>
-            <Text>{message.content}</Text>
-            </View>
-        </View>
-    )
-}
+  const { message } = props;
 
-export default ChatMessage
+  const isMyMessage = () => {
+    return message.user.id === 'u1';
+  };
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.messageBox,
+          { backgroundColor: isMyMessage() ? '#DCF8C5' : 'white' },
+        ]}
+      >
+        <Text style={styles.username}>{message.user.name}</Text>
+        <Text>{message.content}</Text>
+        <Text>{moment(message.createdAt).fromNow()}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ChatMessage;
---
+++ components/ChatMessage/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10
+    },
+    messageBox: {
+        backgroundColor: '#e5e5e5',
+        marginRight: 50,
+        borderRadius: 10,
+        padding: 10,
+    },
+    username: {
+        fontSize: 16
+    }
+})
+
+
+export default styles
